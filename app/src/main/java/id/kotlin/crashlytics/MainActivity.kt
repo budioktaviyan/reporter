b79ef3da1c5a981b1d346dfaef6ada59831d9448
Budi Oktaviyan <budioktaviyans@gmail.com>
2020-07-09 20:04:03 +0700

    Integrate crash report

--- app/src/main/java/id/kotlin/crashlytics/MainActivity.kt
+++ app/src/main/java/id/kotlin/crashlytics/MainActivity.kt
@@ -2,11 +2,26 @@ package id.kotlin.crashlytics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import id.kotlin.crashlytics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivityMainBinding
+  private lateinit var crashlytics: FirebaseCrashlytics
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    crashlytics = FirebaseCrashlytics.getInstance()
+
+    val view = binding.root
+    setContentView(view)
+    setupListener()
+  }
+
+  private fun setupListener() {
+    binding.btnCrash.setOnClickListener { throw RuntimeException("Crash me!") }
   }
 }
